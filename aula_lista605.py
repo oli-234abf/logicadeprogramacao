def98049364ed263bdd9535624713fc0fdf1c48e
Lucia_02 <olimpianna534@gmail.com>
2025-05-06 13:55:27 +0000

    aula_lista605.py

--- aula_lista605.py
+++ aula_lista605.py
@@ -31,7 +31,7 @@
 #     print(compras[1])
 
 #Atividade: faça um programa que possua um vetor denominado A que armazene 6 números inteiros.
-A = [1,2,3,4,5,6]
+# A = [1,2,3,4,5,6]
 
 #a__Atribua os seguintes valores a esse vetor:1,0,5,-2,-5,7
 
@@ -59,21 +59,92 @@ A = [1,2,3,4,5,6]
 # print(A[5])
 
 #leias as notas de uma turma de uma turma de cinco estudantes e depois imprima as notas que são maiores do que a média da turma
+#MEU CÓDIGO:
+# notas = []
 
-notas = []
+# for i in range(5):
+#     nota = float(input("insira nota: "))
+#     notas.append(nota)
 
-for i in range(5):
-    nota = float(input("insira nota: "))
-    notas.append(nota)
+# soma = 0
+# for x in notas:
+#     soma = soma + x
+# media = soma /len(notas)
 
-soma = 0
-for x in notas:
-    soma = soma + x
-media = soma /len(notas)
+# print("as notas são: ")
+# for x in notas:
+#     print(x)
+#     if x > media:
+#         print(x, "maior que a media")
 
-print("as notas são: ")
-for x in notas:
-    print(x)
-print("a media das cinco notas: ",media)
-print("a maior nota: ",max(notas))
-print("a menor nota: ",min(notas))
+# print("a media da nota dos cinco alunos: ",media)
+
+#CODIGO DO PROFESSOR:
+# notas = []
+
+# for i in range(5):
+#     nota = float(input("insira nota: "))
+#     notas.append(nota)
+
+# soma = 0
+# for x in notas:
+#     soma = soma + x
+# media = soma /len(notas)
+
+# for x in notas:
+#     if x > media:
+#         print( "nota maior que a media", x)
+
+# print("a media da nota dos cinco alunos: ",media)
+
+#Para uma lista contendo 5 números inteiros, formular um algorítimo que determine o maior elemento desta lista
+
+# num = [1, 2, 3, 4, 5]
+
+# # print(max(num))
+# # print(min(num))
+
+# maior = num [0]
+# for x in num:
+#     if x > maior:
+#         maior = x
+# print(maior)
+
+# menor = num [0]
+# for x in num:
+#     if x < menor:
+#         menor = x
+# print(menor)
+
+# crie um programa que le 6 valores inteiros e, em seguida, mostre na tela o valores lidos
+# valores = []
+# for i in range(6):
+#     valor = int(input("insira valor: "))
+#     valores.append(valor)
+
+# for numero in valores:
+#     print(numero)
+
+#ler um conjunto de numeros reais, armazenando-os em lista e calcular o quadrado das componentes desta lista, armazenando em outra lista.os
+#conjuntos têm 10 elementos cada.Imprimir todos os conjuntos
+
+num_reais = []
+num_quadrado = []
+for i in range(10):
+    num = float(input("Insira número real: "))
+    num_reais.append(num)
+
+for x in num_reais:
+    quadrado = x * x
+    num_quadrado.append(quadrado)
+print(num_reais)
+print(num_quadrado)
+#tinha que sair do for
+
+
+
+
+
+
+
+
