7b6686ab2727ba4839d261e477a97d7cfd8afa98
Lucia_02 <olimpianna534@gmail.com>
2025-05-13 11:59:55 +0000

    questao_10.py questao_11.py

--- questao_10.py
+++ questao_10.py
@@ -1,4 +1,10 @@
-N = int(input())
+while True:
+    N = int(input("insira N: "))
+    if N > 0 and N % 2 == 0:
+        break
+    else:
+        print("Número inválido")
+
 for x in range(N+1):
     if x % 2 == 0:
         print (x)
